abded17cf7d3f43f0f67cc37fbbb3b81e2058aed
Ariuntuul <ariuntuulenkhtaivan@gmail.com>
2022-03-14 22:58:18 -0500

    US6_Last update

--- src/test/java/com/vytrack/tests/US6.java
+++ src/test/java/com/vytrack/tests/US6.java
@@ -2,6 +2,7 @@ package com.vytrack.tests;
 import com.vytrack.utilities.BrowserUtils;
 import com.vytrack.utilities.ConfigurationReader;
 import com.vytrack.utilities.Driver;
+import com.vytrack.utilities.VyTrackUtils;
 import org.openqa.selenium.*;
 import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
@@ -24,25 +25,18 @@ public class US6 {
         Driver.getDriver().get(ConfigurationReader.getProperty("env1"));
 
         //2.    Users can log in as a store manager
-        WebElement usernameInput = Driver.getDriver().findElement(By.xpath("//input[@id='prependedInput']"));
-        usernameInput.sendKeys("storemanager51");
-
-        WebElement passwordInput = Driver.getDriver().findElement(By.xpath("//input[@id='prependedInput2']"));
-        passwordInput.sendKeys("UserUser123");
-
-        WebElement loginBtn = Driver.getDriver().findElement(By.xpath("//button[@id='_submit']"));
-        loginBtn.click();
+        VyTrackUtils.loginAsStoreManger();
+        BrowserUtils.sleep(2);
 
         //3.	Click the Vehicles under the Fleet
         WebElement fleet = Driver.getDriver().findElement(By.xpath("//ul[@class='nav-multilevel main-menu']/li[2]"));
-        BrowserUtils.sleep(3);
+        BrowserUtils.sleep(2);
         Actions actions = new Actions(Driver.getDriver());
         actions.moveToElement(fleet).perform();
 
-        BrowserUtils.sleep(3);
+        BrowserUtils.sleep(2);
 
         //4.	Verify any car info has 3 dots “...”, and there are “view, edit, delete” icons
-
         WebElement vehicle = Driver.getDriver().findElement(By.xpath("//ul[@class='nav-multilevel main-menu']/li[2]/div/div/ul/li[3]"));
 
         vehicle.click();
